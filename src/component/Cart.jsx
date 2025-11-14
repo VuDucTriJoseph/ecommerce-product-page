bdc8d9162f985959d6b04e2a10540d9e8215062a
VuDucTriJoseph <vutrilkvn89@gmail.com>
2025-11-14 13:55:56 +0700

    cart component done

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -12,31 +12,32 @@ const cartDatas = [
 function Cart() {
   return (
     <div className="cart">
-      <h3 className="cart__title">Cart</h3>
+      <h3 className="cart__title text-pre-3--bold">Cart</h3>
       {cartDatas.length === 0 ? (
-        <p>Your cart is empty</p>
+        <div className="cart__info">
+          <p className="text-pre-3--bold">Your cart is empty</p>
+        </div>
       ) : (
         <div className="cart__info">
           {cartDatas.map((cart) => (
             <div className="cart__item">
-              <img src={cart.image} alt="product image" />
-              <div>
-                <p className="cart__name">
-                  `${cart.name} ${cart.curentcy}$
-                  {(cart.price * cart.discount) / 100} x ${cart.quantity}`
-                </p>
-                <span className="cart__total">
-                  `${cart.curentcy}$
-                  {((cart.price * cart.discount) / 100) * cart.quantity}`
+              <img src={cart.image} alt="product image" className="cart__img" />
+              <p>
+                <span className="cart__name text-pre-3--regular">
+                  {cart.name} {cart.curentcy}
+                  {(cart.price * cart.discount) / 100} x {cart.quantity}
                 </span>
-              </div>
-              <button className="cart__btn--remove">
-                <img
-                  src="/images/icon-delete.svg"
-                  alt="delete button icon"
-                  className="cart__btn--remove-icon"
-                />
-              </button>
+                <span className="cart__total text-pre-3--bold">
+                  {cart.curentcy}
+                  {((cart.price * cart.discount) / 100) * cart.quantity}
+                </span>
+              </p>
+
+              <img
+                src="/images/icon-delete.svg"
+                alt="delete button icon"
+                className="cart__btn--remove-icon"
+              />
             </div>
           ))}
           <button className="cart__checkout">Checkout</button>
